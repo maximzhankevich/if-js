d67cd3f6fd8da9f78eee52174af7571a1cfe756d
maximzhankevich <maximzhankevich@gmail.com>
2021-02-08 13:53:10 +0300

    all homework lesson-3

--- main.js
+++ main.js
@@ -51,4 +51,78 @@ for(let i = 0; i<=arr2.length; i++){
     console.log(arr2[i]);
 }
 
+//Function
+
+//Функция palindrome
+
+function palindrome (str){
+    //debugger
+    let len = str.length;
+    for (i = 0; i < str.length; i++){
+        if (str[i] !== str[len - 1 - i]) {
+            return false
+        }
+        
+    }
+
+    return true
+}
+
+palindrome ("шалаш");
+palindrome ("жыве");
+palindrome ("Беларусь");
+
+//Функция min(a, b) и функция max(a,b)
+
+function min (a, b){
+    //debugger
+    if (a < b){
+        return a
+    }
+    return b
+}
+
+min(88,6);
+min(9,777);
+
+
+function max (a, b){
+    //debugger
+    if (a > b){
+        return a
+    }
+    return b
+}
+
+max(88,6);
+max(9,777);
+
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+
+  function max(a, b) {
+    return a > b ? a : b;
+}
+
+//Замена элементов массива
+
+
+function changeZero1() {
+    let arr = [1,10,2,20,3,30,4,40,5,50];
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] % 10 == 0){
+            arr = arr.join().replace(/0/g, 'zero');
+            console.log(arr.split(','));
+        }
+    }
+}
+
+changeZero1();
+
+
+
+
+
 

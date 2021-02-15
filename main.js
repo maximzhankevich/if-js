ffaa66ed0057eba9cc47f46db2ea6f8e8b838146
maximzhankevich <74614992+maximzhankevich@users.noreply.github.com>
2021-02-15 16:54:42 +0300

    homework lesson-5 (#5)
    Co-authored-by: nikitik-nikita <nik.ikraaa@gmail.com>

--- main.js
+++ main.js
@@ -171,3 +171,69 @@ function changeColor3(e) {
   }
 }
 paragraph3.addEventListener('click', changeColor3);
+
+// String
+
+let date = '2020-11-26';
+
+function changeDate(){
+  let arrDate = date.split('-'); 
+  arrDate = arrDate.reverse().join('.'); 
+  console.log(arrDate);  
+}
+changeDate();
+
+const data = [
+  {
+    country: 'Russia',
+    city: 'Saint Petersburg',
+    hotel: 'Hotel Leopold',
+  },
+  {
+    country: 'Spain',
+    city: 'Santa Cruz de Tenerife',
+    hotel: 'Apartment Sunshine',
+  },
+  {
+    country: 'Slowakia',
+    city: 'Vysokie Tatry',
+    hotel: 'Villa Kunerad',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hostel Friendship',
+  },
+  {
+    country: 'Indonesia',
+    city: 'Bali',
+    hotel: 'Ubud Bali Resort&SPA',
+  },
+  {
+    country: 'Netherlands',
+    city: 'Rotterdam',
+    hotel: 'King Kong Hostel',
+  },
+  {
+    country: 'Marocco',
+    city: 'Ourika',
+    hotel: 'Rokoko Hotel',
+  },
+  {
+    country: 'Germany',
+    city: 'Berlin',
+    hotel: 'Hotel Rehberge Berlin Mitte',
+  },
+];
+
+function search(text) {
+  let result = [];
+  for (let i = 0; i < data.length; i++) {
+    const current = data[i].country + data[i].city + data[i].hotel;
+    if (current.includes(text)) {
+      result.push(data[i]);
+    }
+  }
+  return console.log(result);
+}
+search('Germany');
